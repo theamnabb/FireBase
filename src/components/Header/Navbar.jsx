ef750434337971b06b5bc8963ad4d2edac6a583d
aamna-ansari <aamnansari29@gmail.com>
2024-12-16 20:08:25 +0500

    /

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,35 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
-  <div className="container">
-    <Link to = '/' className="navbar-brand" >LearnFireBase</Link >
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-    <Link to = '/' className="nav-link" >Home</Link >
-        </li>
-        <li className="nav-item">
-        <Link to = 'about/' className="nav-link" >About</Link >
-        </li>
-        <li className="nav-item">
-        <Link to = 'contact/' className="nav-link" >Contact</Link >
-        </li>
-      </ul>
-      <div className="d-flex">
-      <Link to = '/auth/login' className="btn btn-success" >Login</Link >
-      </div>
-    </div>
-  </div>
-</nav>
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            LearnFireBase
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="about/" className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="contact/" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className="d-flex">
+              <Link to="/auth/login" className="btn btn-success">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
